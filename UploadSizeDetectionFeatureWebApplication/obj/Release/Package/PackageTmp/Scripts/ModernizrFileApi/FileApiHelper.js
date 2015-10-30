1530ce20e3821278d8ac0de8c5e1f5902227bd5f
Rodolfo Guluarte <guluarte@gmail.com>
2015-10-30 12:06:16 -0700

    Custom solution working with HTML5 and Active X as fallback

---
+++ UploadSizeDetectionFeatureWebApplication/obj/Release/Package/PackageTmp/Scripts/ModernizrFileApi/FileApiHelper.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="jquery-1.10.2.intellisense.js" />
+/// <reference path="../modernizr-2.6.2.js" />
+
+$(function () {
+
+    var Mb = 1048576;
+    var maxFileSize = Mb * 1;
+
+    var fileInputId = "file";
+
+    var detectFileSize = function (elementId) {
+
+        var element = document.getElementById(elementId);
+
+        // If supported use the HTML5 API
+        if (window.FileReader || "FileReader" in window || typeof FileReader !== "undefined") {
+            return element.files[0].size;
+        }
+
+        // Fallback to ActiveX
+        if (window.ActiveXObject) {
+
+            var fso = new ActiveXObject("Scripting.FileSystemObject");
+            var filepath = element.value;
+            var thefile = fso.getFile(filepath);
+            return thefile.size;
+        }
+
+        return -1;
+
+    };
+
+    var bytesToMegabytes = function(bytes) {
+        return bytes / Mb;
+    }
+
+    $("#" + fileInputId).bind("change", function () {
+
+        console.log(detectFileSize(fileInputId));
+
+        var uploadFileSize = detectFileSize(fileInputId);
+
+        if (uploadFileSize > maxFileSize) {
+            $("#result").html("The file is too big: " + bytesToMegabytes(uploadFileSize) + " Mb");
+            $("#submit").attr('disabled', 'disabled');
+        } else {
+            $("#result").html("OK: " + bytesToMegabytes(uploadFileSize) + " Mb");
+            $("#submit").removeAttr('disabled');
+        }
+
+    });
+});
---
+++ UploadSizeDetectionFeatureWebApplication/obj/Release/Package/PackageTmp/Scripts/custom.js
@@ -0,0 +1,99 @@
+﻿/// <reference path="jquery-1.10.2.intellisense.js" />
+
+$(function () {
+
+    var uploadUrl = "/Home/Upload";
+    var maxUploadSize = "1mb";
+
+    var defaultFilters = {
+        max_file_size: maxUploadSize,
+        mime_types: [
+            { title: "Image files", extensions: "jpg,gif,png" },
+            { title: "Zip files", extensions: "zip" }
+        ]
+    };
+
+    // Setup html5 version
+    $("#uploader").pluploadQueue({
+        // General settings
+        runtimes: 'html5,flash,silverlight,html4',
+        url: uploadUrl,
+        rename: true,
+        dragdrop: true,
+
+        filters: defaultFilters,
+
+        // Resize images on clientside if we can
+        resize: {
+            width: 200,
+            height: 200,
+            quality: 90,
+            crop: true // crop to exact dimensions
+        },
+
+
+        // Flash settings
+        flash_swf_url: '/Scripts/Moxie.swf',
+
+        // Silverlight settings
+        silverlight_xap_url: '/Scripts/Moxie.xap'
+    });
+
+    // All runtimes demo
+    // Setup flash version
+    $("#flash_uploader").pluploadQueue({
+        // General settings
+        runtimes: 'flash',
+        url: uploadUrl,
+        unique_names: true,
+
+        filters: defaultFilters,
+
+        // Resize images on clientside if we can
+        resize: { width: 320, height: 240, quality: 90 },
+
+        // Flash settings
+        flash_swf_url: '/Scripts/Moxie.swf'
+    });
+
+
+    // Setup silverlight version
+    $("#silverlight_uploader").pluploadQueue({
+        // General settings
+        runtimes: 'silverlight',
+        url: uploadUrl,
+        unique_names: true,
+
+        filters: defaultFilters,
+
+        // Resize images on clientside if we can
+        resize: { width: 320, height: 240, quality: 90 },
+
+        // Silverlight settings
+        silverlight_xap_url: '/Scripts/Moxie.xap'
+    });
+
+    // Setup html5 version
+    $("#html5_uploader").pluploadQueue({
+        // General settings
+        runtimes: 'html5',
+        url: uploadUrl,
+        unique_names: true,
+
+        filters: defaultFilters,
+
+        // Resize images on clientside if we can
+        resize: { width: 320, height: 240, quality: 90 }
+    });
+
+
+    // Setup html4 version
+    $("#html4_uploader").pluploadQueue({
+        // General settings
+        runtimes: 'html4',
+        url: uploadUrl,
+        unique_names: true,
+
+        filters: defaultFilters
+    });
+});
